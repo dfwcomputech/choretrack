589295dc25cfa045d764dc501da9257fb043fc4a
mcumbay <miguel.cumbay@live.com>
2023-08-21 17:52:59 -0500

    Initial Commit:
    The following libraries were added:
    1. Spring Boot 3.1.2
    2. Java 20
    3. Thymeleaf
    4. Bootstrap
    5. Htmx

---
+++ choretrack-ui/src/main/java/com/computech/ctui/ChoretrackUiApplication.java
@@ -0,0 +1,13 @@
+package com.computech.ctui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoretrackUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoretrackUiApplication.class, args);
+	}
+
+}
---
+++ choretrack-ui/src/test/java/com/computech/ctui/ChoretrackUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.computech.ctui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoretrackUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
